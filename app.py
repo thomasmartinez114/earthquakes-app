ab9bee97328e71b5d32bcc9ebbbde433b9553755
Thomas Martinez <devtommie114@gmail.com>
2024-07-06 13:51:27 -0400

    install pandas to read files / uploading csv and pandas showing on ui

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Earthquake Data Explorer')
 st.text('This is a web app to explore earthquake data.')
 #st.markdown('## This is **markdown**')
 
 uploaded_file = st.file_uploader('Upload your file here')
+
+if uploaded_file:
+    # pandas to read the file
+    data = pd.read_csv(uploaded_file)
+
+    # display the information
+    st.write(data.describe())
