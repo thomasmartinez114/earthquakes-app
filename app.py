a050fa23f226fe06b1196e1cf2d3fe2a88e69223
Thomas Martinez <devtommie114@gmail.com>
2024-07-06 12:01:26 -0400

    added file uploader

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title('Earthquake Data Explorer')
+st.text('This is a web app to explore earthquake data.')
+#st.markdown('## This is **markdown**')
+
+uploaded_file = st.file_uploader('Upload your file here')
