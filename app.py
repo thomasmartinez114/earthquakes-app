99046abe039dd2f37e267c289c7e7c69ddb60341
Thomas Martinez <devtommie114@gmail.com>
2024-07-06 13:58:39 -0400

    added second table

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ st.text('This is a web app to explore earthquake data.')
 uploaded_file = st.file_uploader('Upload your file here')
 
 if uploaded_file:
+
+    st.header("Data Statistics")
     # pandas to read the file
     data = pd.read_csv(uploaded_file)
 
     # display the information
     st.write(data.describe())
+
+    st.header('Data Header')
+    st.write(data.head())
