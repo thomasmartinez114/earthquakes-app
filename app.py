faa1c07ad9b981d42b662e7b028bffa9faac043f
Thomas Martinez <devtommie114@gmail.com>
2024-07-07 11:04:57 -0400

    pages created and functional

--- app.py
+++ app.py
@@ -2,6 +2,24 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def stats(dataframe):
+    st.header('Data Statistics')
+    st.write(dataframe.describe())
+
+def data_header(dataframe):
+    st.header('Data Header')
+    st.write(data.head())
+
+def plot(dataframe):
+    #graph plot
+    fig, ax = plt.subplots(1,1)
+    ax.scatter(x=data['Depth'], y=data['Magnitude'])
+    ax.set_xlabel('Depth')
+    ax.set_ylabel('Magnitude')
+
+    st.pyplot(fig)
+
+
 st.title('Earthquake Data Explorer')
 st.text('This is a web app to explore earthquake data.')
 #st.markdown('## This is **markdown**')
@@ -11,22 +29,17 @@ st.sidebar.title("Navigation")
 
 uploaded_file = st.sidebar.file_uploader('Upload your file here')
 
-if uploaded_file:
+options = st.sidebar.radio('Pages', options=['Home', 'Data Statistics', 'Data Header', 'Plot'])
 
-    st.header("Data Statistics")
+# File uploader
+if uploaded_file:
     # pandas to read the file
     data = pd.read_csv(uploaded_file)
 
-    # display the information
-    st.write(data.describe())
-
-    st.header('Data Header')
-    st.write(data.head())
-
-    #graph plot
-    fig, ax = plt.subplots(1,1)
-    ax.scatter(x=data['Depth'], y=data['Magnitude'])
-    ax.set_xlabel('Depth')
-    ax.set_ylabel('Magnitude')
-
-    st.pyplot(fig)
+# Render pages
+if options == 'Data Statistics':
+    stats(data)
+elif options == 'Data Header':
+    data_header(data)
+elif options == 'Plot':
+    plot(data)
